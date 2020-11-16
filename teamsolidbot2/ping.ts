e15c6616d14e898493a61197e7147e12590953b9
mogoh <mv@mogoh.de>
2020-11-16 18:17:01 +0100

    Update Ping.ts

--- teamsolidbot2/ping.ts
+++ teamsolidbot2/ping.ts
@@ -1,43 +1,22 @@
-import { Client, Message, Intents } from 'https://raw.githubusercontent.com/harmony-org/harmony/main/mod.ts'
+import { Client, Message, Intents } from 'https://raw.githubusercontent.com/harmony-org/harmony/main/mod.ts';
 
-const client = new Client()
+import { TOKEN } from './config.ts';
+import { INTENTS } from './config.ts';
+
+const client = new Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user?.tag}!`)
+    console.log(`Logged in as ${client.user?.tag}!`);
 })
 
 client.on('messageCreate', (msg: Message) => {
     if (msg.content === '!ping') {
-        console.log('Command Used: Ping')
-        msg.reply('pong!')
+        console.log('Command Used: Ping');
+        msg.reply('pong!');
     }
 })
 
-console.log('harmony - ping example')
+console.log('harmony - ping example');
+console.log(TOKEN.length);
 
-const token = prompt('Input Bot Token:')
-if (token === null) {
-    console.log('No token provided')
-    Deno.exit()
-}
-
-const intents = prompt(
-    'Input Intents (0 = All, 1 = Presence, 2 = Server Members, 3 = None):'
-)
-if (intents === null || !['0', '1', '2', '3'].includes(intents)) {
-    console.log('No intents provided')
-    Deno.exit()
-}
-
-let ints
-if (intents === '0') {
-    ints = Intents.All
-} else if (intents === '1') {
-    ints = Intents.Presence
-} else if (intents === '2') {
-    ints = Intents.GuildMembers
-} else {
-    ints = Intents.None
-}
-
-client.connect(token, ints)
+client.connect(TOKEN, INTENTS);
