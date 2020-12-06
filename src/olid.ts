12af4da44446469e5dec2a9e32742678fbc1e739
mogoh <mv@mogoh.de>
2020-12-06 20:13:39 +0100

    Update Olid

--- src/olid.ts
+++ src/olid.ts
@@ -1,7 +1,13 @@
 import { Message } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
 import { config } from './config.js';
 import { log_sended_message } from './log.js';
 import { choice } from './utils.js';
+
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IOlid extends Array<string> {}
 
 
 const command = 'olid';
@@ -9,28 +15,24 @@ const c = 'o';
 export const olid_help =
     `${config.prefix}${command}/${config.prefix}${c} - Greet with a friendly: “Olid!”`;
 
-const olidlist: string[] = [
-    'Olid!',
-    'OLID!',
-    'O L I D',
-    'O L I D!',
-    'olid',
-    'olaf',
-    'OLID',
-    '*Olid*',
-    '**olid**',
-    '***OlId***',
-    // '```\n' +
-    // rektangle('OLID', randint(2, 6), randint(2, 6)) +
-    // '```\n',
-    '__olid__',
-    'https://i.imgur.com/JW6YLy9.jpg',
-];
+
+// Load
+const file_name = 'olid.json';
+const absolute_olid_path: string = path.resolve(config.assets_dir, file_name);
+let file_string: string;
+let olid_data: IOlid;
+if (fs.existsSync(absolute_olid_path)) {
+    file_string = fs.readFileSync(absolute_olid_path, 'utf-8');
+    olid_data = JSON.parse(file_string) as IOlid;
+} else {
+    olid_data = [];
+}
 
 
 export function olid(message: Message): void {
     if (message.content.trim() === `${config.prefix}${command}` || message.content.trim() === `${config.prefix}${c}`) {
-        message.channel.send(choice(olidlist))
+        const olid_string = choice(olid_data) ?? 'I would say something like “olid” or so, but **SOMEONE** is just not able to cofigure properly. 🤦';
+        message.channel.send(olid_string)
             .then(log_sended_message)
             .catch(console.error);
     }
