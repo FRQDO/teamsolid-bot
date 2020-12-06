059cd567eb09b73488900d4d472164bc540b19b4
mogoh <mv@mogoh.de>
2020-12-06 20:14:19 +0100

    Update Yes NO

--- src/yes_no.ts
+++ src/yes_no.ts
@@ -17,7 +17,7 @@ export const yes_no_help =
 
 // Load
 const file_name = 'yes_no.json';
-const absolute_yes_no_path: string = path.resolve(config.data_dir, file_name);
+const absolute_yes_no_path: string = path.resolve(config.assets_dir, file_name);
 let file_string: string;
 let yes_no_data: IYesNo;
 if (fs.existsSync(absolute_yes_no_path)) {
@@ -29,7 +29,8 @@ if (fs.existsSync(absolute_yes_no_path)) {
 
 export function yes_no(message: Message): void {
     if (message.content.trim() === `${config.prefix}${command}` || message.content.trim() === `${config.prefix}${c}`) {
-        message.channel.send(choice(yes_no_data))
+        const quote = choice(yes_no_data) ?? 'I can\'t tell you your fortune, because **SOMEONE** forgot to configure `yes_no.json` properly.';
+        message.channel.send(quote)
             .then(log_sended_message)
             .catch(console.error);
     }
