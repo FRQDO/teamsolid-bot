fe03e3c5c09f21ee8aa1f9ba2a53cf0b6427c73a
mogoh <mv@mogoh.de>
2020-12-06 23:16:48 +0100

    Update markov.ts

--- src/markov.ts
+++ src/markov.ts
@@ -1,4 +1,21 @@
-import { MarkovString } from 'ts-markov';
+import { Message } from 'discord.js';
+import { config } from './config.js';
+import { MarkovString } from './ts-markov/dist/MarkovString';
 
 
 new MarkovString();
+
+
+const command = 'markov';
+const c = 'm';
+export const olid_help =
+    `${config.prefix}${command}/${config.prefix}${c} - Whispers some wisdom of the channel.`;
+
+
+// Load
+// TODO
+
+export function markov(message: Message): void {
+    console.log(message);
+    // TODO
+}
